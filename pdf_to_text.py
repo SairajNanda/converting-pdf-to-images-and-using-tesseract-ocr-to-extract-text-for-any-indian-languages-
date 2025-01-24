a0c0b64550a52cf35d0bb4c349c828aca47f825c
sairaj nanda <106545126+SairajNanda@users.noreply.github.com>
2025-01-24 22:49:16 +0530

    Add files via upload

---
+++ pdf_to_text.py
@@ -0,0 +1,19 @@
+from pdf2image import convert_from_path
+
+
+def convert_pdf(pdf_path,save_directory,res=400):
+    pages = convert_from_path(pdf_path,res)
+
+    name_with_extension = pdf_path.rsplit('/')[-1]
+    name = name_with_extension.rsplit('.')[0]
+
+    for idx,page in enumerate(pages):
+        page.save(f'{save_directory}/{name}_{idx}.png','PNG')
+
+convert_pdf('/home/speech/Desktop/bodo2.pdf', '/home/speech/Desktop/bodo_scraped/bodopdf2image')
+
+
+# John Wesly G, Thu 14:04
+# sudo apt install tesseract-ocr
+# sudo apt-get install tesseract-ocr*
+# tesseract ~/path/image.png - -l ben
